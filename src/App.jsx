85461f6d0879dcd91ded26da8d10325090cfc583
Pravin Kadam <pravinkadam21998@gmail.com>
2024-03-06 20:12:43 +0530

    TMDB Api setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,22 @@
 import './App.css'
+import { useState,useEffect } from 'react'
+import { fetchDataFromApi } from "./utils/api.js"
+
 
 function App() {
+
+  useEffect(()=>{
+    apiTesting()
+  },[])
+
+const apiTesting=(req,res)=>{
+
+  fetchDataFromApi("/movie/popular")
+ .then((res)=>{
+  console.log(res);
+}
+)}
+
   return (
     <>
      App
